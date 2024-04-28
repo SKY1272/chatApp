a8f7d0450b3dedc9c43a8cbbdc69537b0e5ff6f2
SKY1272 <MY_NAME@example.com>
2024-04-28 17:22:04 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static('public'));
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'login.html'));
+});
+
+app.post('/login', (req, res) => {
+    const { username } = req.body;
+    if (username) {
+        res.cookie('username', username);
+        res.redirect('/');
+    } else {
+        res.send('Please enter a valid username.');
+    }
+});
+
+app.get('/', (req, res) => {
+    const username = req.cookies.username;
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.post('/message', (req, res) => {
+    const username = req.cookies.username;
+    const message = req.body.message;
+    if (username && message) {
+        const data = `${username}: ${message}\n`;
+        fs.appendFile('messages.txt', data, (err) => {
+            if (err) throw err;
+            console.log('Message saved to file.');
+        });
+        res.redirect('/');
+    } else {
+        res.send('Please enter a message.');
+    }
+});
+
+app.get('/messages', (req, res) => {
+    fs.readFile('messages.txt', 'utf8', (err, data) => {
+        if (err) throw err;
+        const messages = data.split('\n');
+        res.send(messages);
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
